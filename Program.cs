19e3331eb906dc06c024fa2155c254a6e3140519
DatSpace <konkarapas@outlook.com>
2017-01-30 21:56:02 +0000

    Many checks/restrictions, optional time!
    ChangeLog:
    
    1)Restrict year: 2017-2099.
    2)Restrict months: 1-12.
    3)Restrict days per month (even for leap years).
    4)Restrict hours: 0-23.
    5)Restrict minutes: 0-59.
    6)Restrict seconds: 0-59.
    7)Make option to use time or not.
    
    (Next commit is going to be optimizing the code and trying to siplify
    it)

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TimeToCalculator
 {
@@ -20,77 +16,315 @@ namespace TimeToCalculator
             char[] dateChar = dateTo.ToCharArray();
             char[] timeChar = timeTo.ToCharArray();
 
+            //START OF DATE CHECKING
+
             while (validDate == false)
             {
                 if (dateTo.Length != 10 ||
                     (dateChar[2] != '/' || dateChar[5] != '/'))
                 {
                     if (again == true)
-                        Console.WriteLine("\n\nPlease use the correct date format...");
-                    Console.WriteLine("Please enter the date-to (dd/mm/yyyy)...");
-                    Console.WriteLine("Example: 05/10/2017...");
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\n\nPlease use the correct date format");
+                        Console.ResetColor();
+                    }
+                    
+                    Console.WriteLine("\nPlease enter the date-to (dd/mm/yyyy)");
+                    Console.WriteLine("Example: 05/10/2017");
                     Console.Write("\nPlease enter the date: ");
                     again = true;
                     dateTo = Console.ReadLine();
                     Console.WriteLine();
                     dateChar = dateTo.ToCharArray();
                 }
+                else if (Convert.ToInt32(dateTo.Substring(6, 4)) > 2099 || Convert.ToInt32(dateTo.Substring(6, 4)) < 2017)
+                {
+                    again = false;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please use a valid year (2017-2099)");
+                    Console.ResetColor();
+                    
+                    Console.Write("\nPlease enter the date: ");
+                    dateTo = Console.ReadLine();
+                    Console.WriteLine();
+                }
+                else if (Convert.ToInt32(dateTo.Substring(3, 2)) > 12 || Convert.ToInt32(dateTo.Substring(3, 2)) < 1)
+                {
+                    again = false;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please use a valid month (01-12)");
+                    Console.ResetColor();
+                    
+                    Console.Write("\nPlease enter the date: ");
+                    dateTo = Console.ReadLine();
+                    Console.WriteLine();
+                }
                 else
+                {
                     validDate = true;
+                    if (Convert.ToInt32(dateTo.Substring(3, 2)) != 2)
+                    {
+                        if (Convert.ToInt32(dateTo.Substring(3, 2)) <= 7)
+                        {
+                            if (Convert.ToInt32(dateTo.Substring(3, 2)) % 2 == 1)
+                            {
+                                if (Convert.ToInt32(dateTo.Substring(0, 2)) < 1 || Convert.ToInt32(dateTo.Substring(0, 2)) > 31)
+                                {
+                                    validDate = false;
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Please enter a valid day number for this month (01-31)");
+                                    Console.ResetColor();
+                                    
+                                    Console.Write("\nPlease enter the date: ");
+                                    dateTo = Console.ReadLine();
+                                    Console.WriteLine();
+                                }
+                            }
+                            else
+                            {
+                                if (Convert.ToInt32(dateTo.Substring(0, 2)) < 1 || Convert.ToInt32(dateTo.Substring(0, 2)) > 30)
+                                {
+                                    validDate = false;
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Please enter a valid day number for this month (01-30)");
+                                    Console.ResetColor();
+                                    
+                                    Console.Write("\nPlease enter the date: ");
+                                    dateTo = Console.ReadLine();
+                                    Console.WriteLine();
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (Convert.ToInt32(dateTo.Substring(3, 2)) % 2 == 0)
+                            {
+                                if (Convert.ToInt32(dateTo.Substring(0, 2)) < 1 || Convert.ToInt32(dateTo.Substring(0, 2)) > 31)
+                                {
+                                    validDate = false;
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Please enter a valid day number for this month (0-31)");
+                                    Console.ResetColor();
+                                    
+                                    Console.Write("\nPlease enter the date: ");
+                                    dateTo = Console.ReadLine();
+                                    Console.WriteLine();
+                                }
+                            }
+                            else
+                            {
+                                if (Convert.ToInt32(dateTo.Substring(0, 2)) < 1 || Convert.ToInt32(dateTo.Substring(0, 2)) > 30)
+                                {
+                                    validDate = false;
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Please enter a valid day number for this month (0-30)");
+                                    Console.ResetColor();
+                                    
+                                    Console.Write("\nPlease enter the date: ");
+                                    dateTo = Console.ReadLine();
+                                    Console.WriteLine();
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (Convert.ToInt32(dateTo.Substring(6, 4)) % 4 == 0)
+                        {
+                            if (Convert.ToInt32(dateTo.Substring(0, 2)) < 1 || Convert.ToInt32(dateTo.Substring(0, 2)) > 29)
+                            {
+                                validDate = false;
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("This is a leap year. Please use a valid number of days (01-29)");
+                                Console.ResetColor();
+                                
+                                Console.Write("\nPlease enter the date: ");
+                                dateTo = Console.ReadLine();
+                                Console.WriteLine();
+                            }
+                        }
+                        else if (Convert.ToInt32(dateTo.Substring(0, 2)) < 1 || Convert.ToInt32(dateTo.Substring(0, 2)) > 28)
+                        {
+                            validDate = false;
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("This is not a leap year. Please use a valid number of days (01-28)");
+                            Console.ResetColor();
+                            
+                            Console.Write("\nPlease enter the date: ");
+                            dateTo = Console.ReadLine();
+                            Console.WriteLine();
+                        }
+                    }
+
+                }
+                again = false;
+            }
+
+            //END OF DATE CHECKING
+
+
+            //START OF OPTIONAL TIME QUESTION AND VALIDITY CHECKING
+
+            Console.Clear();
+            string useTime = "";
+            again = false;
+
+            while (useTime.ToUpper() != "Y" && useTime.ToUpper() != "N")
+            {
+                if (again == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n\nPlease type Y (Yes) or N (No)!");
+                    Console.ResetColor();
+                }
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("\nYou chose date: {0}\n", dateTo);
+                Console.ResetColor();
+                
+                Console.WriteLine("Would you like to specify the time of the date you chose, as well ?");
+                Console.Write("Specify time (Y/N): ");
+                useTime = Console.ReadLine();
+                again = true;
             }
 
             again = false;
 
-            while (validTime == false)
+            if (useTime.ToUpper() == "Y")
             {
-                if (timeTo.Length != 8 ||
-                    (timeChar[2] != ':' || timeChar[5] != ':'))
+                while (validTime == false)
                 {
-                    if (again == true)
-                        Console.WriteLine("\n\nPlease use the correct time format...");
-                    Console.WriteLine("Please enter the time-to (hh:mm:ss)");
-                    Console.WriteLine("Example: 17:23:46 !");
-                    Console.Write("\nPlease enter the time: ");
-                    again = true;
-                    timeTo = Console.ReadLine();
-                    Console.WriteLine();
-                    timeChar = timeTo.ToCharArray();
+                    if (timeTo.Length != 8 ||
+                        (timeChar[2] != ':' || timeChar[5] != ':'))
+                    {
+                        if (again == true)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\n\nPlease use the correct time format");
+                            Console.ResetColor();
+                        }
+                            
+                        Console.WriteLine("\nPlease enter the time-to (hh:mm:ss)");
+                        Console.WriteLine("Example: 17:23:46");
+                        Console.Write("\nPlease enter the time: ");
+                        again = true;
+                        timeTo = Console.ReadLine();
+                        Console.WriteLine();
+                        timeChar = timeTo.ToCharArray();
+                    }
+                    else if (Convert.ToInt32(timeTo.Substring(0,2)) < 0 || Convert.ToInt32(timeTo.Substring(0,2)) > 23)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Please enter a valid hours number (00-23)");
+                        Console.ResetColor();
+                        
+                        Console.Write("\nPlease enter the time: ");
+                        again = true;
+                        timeTo = Console.ReadLine();
+                        Console.WriteLine();
+                        timeChar = timeTo.ToCharArray();
+                    }
+                    else if (Convert.ToInt32(timeTo.Substring(3, 2)) < 0 || Convert.ToInt32(timeTo.Substring(3, 2)) > 59)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Please enter a valid minutes number (00-59)");
+                        Console.ResetColor();
+                        
+                        Console.Write("\nPlease enter the time: ");
+                        again = true;
+                        timeTo = Console.ReadLine();
+                        Console.WriteLine();
+                        timeChar = timeTo.ToCharArray();
+                    }
+                    else if (Convert.ToInt32(timeTo.Substring(6, 2)) < 0 || Convert.ToInt32(timeTo.Substring(6, 2)) > 59)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Please enter a valid seconds number (00-59)");
+                        Console.ResetColor();
+                        
+                        Console.Write("\nPlease enter the time: ");
+                        again = true;
+                        timeTo = Console.ReadLine();
+                        Console.WriteLine();
+                        timeChar = timeTo.ToCharArray();
+                    }
+                    else
+                        validTime = true;
                 }
-                else
-                    validTime = true;
             }
 
-            DateTime futureDate = new DateTime(Convert.ToInt32(dateTo.Substring(6, 4)), Convert.ToInt32(dateTo.Substring(3, 2)),
-                Convert.ToInt32(dateTo.Substring(0, 2)), Convert.ToInt32(timeTo.Substring(0, 2)),
-                Convert.ToInt32(timeTo.Substring(3, 2)), Convert.ToInt32(timeTo.Substring(6, 2)));
+            //END OF OPTIONAL TIME QUESTION AND VALIDITY CHECKING
 
-            int finalYears = futureDate.Subtract(DateTime.Now).Days / 365;
-            int finalMonths = futureDate.Subtract(DateTime.Now).Days / 30;
-            int finalDays = futureDate.Subtract(DateTime.Now).Days;
-            int finalHours = futureDate.Subtract(DateTime.Now).Hours;
-            int finalMinutes = futureDate.Subtract(DateTime.Now).Minutes;
-            int finalSeconds = futureDate.Subtract(DateTime.Now).Seconds;
+
+            int finalYears = 0;
+            int finalMonths = 0;
+            int finalDays = 0;
+            int finalHours = 0;
+            int finalMinutes = 0;
+            int finalSeconds = 0;
+
+
+            if (useTime.ToUpper() == "Y")
+            {
+                DateTime futureDate = new DateTime(Convert.ToInt32(dateTo.Substring(6, 4)), Convert.ToInt32(dateTo.Substring(3, 2)),
+                    Convert.ToInt32(dateTo.Substring(0, 2)), Convert.ToInt32(timeTo.Substring(0, 2)),
+                    Convert.ToInt32(timeTo.Substring(3, 2)), Convert.ToInt32(timeTo.Substring(6, 2)));
+                finalYears = futureDate.Subtract(DateTime.Now).Days / 365;
+                finalMonths = futureDate.Subtract(DateTime.Now).Days / 30;
+                finalDays = futureDate.Subtract(DateTime.Now).Days;
+                finalHours = futureDate.Subtract(DateTime.Now).Hours;
+                finalMinutes = futureDate.Subtract(DateTime.Now).Minutes;
+                finalSeconds = futureDate.Subtract(DateTime.Now).Seconds;
+            }
+            else
+            {
+                DateTime futureDate = new DateTime(Convert.ToInt32(dateTo.Substring(6, 4)), 
+                    Convert.ToInt32(dateTo.Substring(3, 2)), Convert.ToInt32(dateTo.Substring(0, 2)));
+                finalYears = futureDate.Subtract(DateTime.Now).Days / 365;
+                finalMonths = futureDate.Subtract(DateTime.Now).Days / 30;
+                finalDays = futureDate.Subtract(DateTime.Now).Days;
+            }
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("\nThe calculations show that there are:\n");
+
+            Console.ResetColor();
 
             if (finalYears > 0)
             {
-                Console.Write("Years left: -{0}- / ", finalYears);
+                Console.WriteLine("{0}\tyears left.", finalYears);
                 finalMonths -= 12 * finalYears;
             }
             if (finalMonths > 0)
             {
-                Console.Write("Months left: -{0}- / ", finalMonths);
+                Console.WriteLine("{0}\tmonths left.", finalMonths);
                 finalDays -= (30 * finalMonths) + (30 * 12 * finalYears);
             }
             if (finalDays > 0)
-                Console.Write("Days left: -{0}- / ", finalDays);
+                Console.WriteLine("{0}\tdays left.", finalDays);
             if (finalHours > 0)
-                Console.Write("Hours left: -{0}- / ", finalHours);
+                Console.WriteLine("{0}\thours left.", finalHours);
             if (finalMinutes > 0)
-                Console.Write("Minutes left: -{0}- / ", finalMinutes);
+                Console.WriteLine("{0}\tminutes left.", finalMinutes);
             if (finalSeconds > 0)
-                Console.Write("Seconds left: -{0}-", finalSeconds);
+                Console.WriteLine("{0}\tseconds left.", finalSeconds);
 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\n\n\t\t\t\tThank you for using TimeTo! (Press enter to exit)");
             Console.ReadLine();
         }
     }
 }
+
+
+/* 1)Restrict year: 2017-2099. +
+ * 2)Restrict months: 1-12. +
+ * 3)Restrict days per month (even for leap years). +
+ * 4)Restrict hours: 0-23 +
+ * 5)Restrict minutes: 0-59 +
+ * 6)Restrict seconds: 0-59 +
+ * 7)Make option to use time or not. +
+ * 8)
+ */
